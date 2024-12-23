30d3a32e4c9c3b478740d74369ef7a5b5acee559
shafadeascr16 <shafadea11@gmail.com>
2024-12-23 12:57:30 +0700

    First Commit

---
+++ app/src/main/java/com/informatika/doneit/data/repository/AuthRepository.kt
@@ -0,0 +1,14 @@
+package com.informatika.doneit.data.repository
+
+import com.informatika.doneit.data.model.User
+import com.informatika.doneit.util.UiState
+
+interface AuthRepository {
+    fun registerUser(email: String, password: String, user: User, result: (UiState<String>) -> Unit)
+    fun updateUserInfo(user: User, result: (UiState<String>) -> Unit)
+    fun loginUser(email: String, password: String, result: (UiState<String>) -> Unit)
+    fun forgotPassword(email: String, result: (UiState<String>) -> Unit)
+    fun logout(result: () -> Unit)
+    fun storeSession(id: String, result: (User?) -> Unit)
+    fun getSession(result: (User?) -> Unit)
+}
---
+++ app/src/main/java/com/informatika/doneit/data/repository/TaskRepository.kt
@@ -0,0 +1,14 @@
+package com.informatika.doneit.data.repository
+
+import com.informatika.doneit.data.model.Task
+import com.informatika.doneit.data.model.User
+import com.informatika.doneit.util.UiState
+
+interface TaskRepository {
+    fun addTask(task: Task, result: (UiState<Pair<Task,String>>) -> Unit)
+    fun updateTask(task: Task, result: (UiState<Pair<Task,String>>) -> Unit)
+    fun deleteTask(task: Task, result: (UiState<Pair<Task,String>>) -> Unit)
+    fun getTask(id: String, result: (UiState<Pair<Task,String>>) -> Unit)
+    fun getTasks(user: User?, result: (UiState<List<Task>>) -> Unit)
+    fun storeTasks(tasks: List<Task>, result: (UiState<String>) -> Unit)
+}
